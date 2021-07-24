e20908f41846559fba353a406aef34070ca9c892
huchi <huchinlp@gmail.com>
2021-07-24 11:42:33 +0800

    replace the commands for CPU with parallel

--- data/run.sh
+++ data/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-num_processes=18
 
+NUM_THREAD=18
 HARDWARE=$1
 TASK=$2
 
@@ -16,21 +16,8 @@ if [ "$HARDWARE" == "GPU" ]; then
     ./bin/NiuTensor -dev 7 -fp16 1 -model ./model/model.fp16 -srcvocab ./model/vocab.txt -tgtvocab ./model/vocab.txt < nts.tmp.bpe | parallel --pipe -L 4000 -N1 --keep-order "sed -r 's/(@@ )|(@@ ?$)//g' | perl ./moses/detokenizer.perl -q -l de"
 else
     total=`awk 'END{print NR}' nts.tmp.bpe`
-    lines=`expr $total / $num_processes + 1`
-    num=`expr $total / $lines`
-    if [ "$num_processes" -gt "$num" ]; then
-        num_processes=$num
-    fi
-    split -l $lines nts.tmp.bpe -d -a 2 nts.tmp.bpe.
-    num=`expr $num_processes - 1`
-    for ((i=0;i<$num_processes;i++)); do
-    {
-        printf -v ii "%02d" $i
-        ./bin/NiuTensor -dev -1 -model ./model/model.fp32 -srcvocab ./model/vocab.txt -tgtvocab ./model/vocab.txt < nts.tmp.bpe.$ii | parallel --pipe -L 4000 -N1 --keep-order "sed -r 's/(@@ )|(@@ ?$)//g' | perl ./moses/detokenizer.perl -q -l de" > nts.tmp.res.$ii
-    } &
-    done
-    wait
-    cat nts.tmp.res.*
+    lines=`expr $total / $NUM_THREAD + 1`
+    <nts.tmp.bpe parallel --pipe -L $lines -N1 --keep-order "./bin/NiuTensor -dev -1 -model ./model/model.fp32 -srcvocab ./model/vocab.txt -tgtvocab ./model/vocab.txt | sed -r 's/(@@ )|(@@ ?$)//g' | perl ./moses/detokenizer.perl -q -l de"
 fi
 
 rm -rf nts.tmp*
