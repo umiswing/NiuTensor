73f5467f59969077c1c44b9287f99bdcd384a2a6
huchi <huchinlp@gmail.com>
2021-07-28 15:59:05 +0800

    set the gpu device as 0

--- data/run.sh
+++ data/run.sh
@@ -12,7 +12,7 @@ cat | parallel --pipe -L 4000 --keep-order "./nts-scripts/mosestokenizer -N -U e
 
 # Translate
 if [ "$HARDWARE" == "GPU" ]; then
-    ./bin/NiuTensor -dev 6 -fp16 1 -model ./model/model.fp16 -srcvocab ./model/vocab.txt -tgtvocab ./model/vocab.txt -sbatch 3072 -wbatch 64000 < nts.tmp.bpe | parallel --pipe -L 4000 --keep-order "perl ./nts-scripts/detokenizer.perl -q -l de"
+    ./bin/NiuTensor -dev 0 -fp16 1 -model ./model/model.fp16 -srcvocab ./model/vocab.txt -tgtvocab ./model/vocab.txt -sbatch 3072 -wbatch 64000 < nts.tmp.bpe | parallel --pipe -L 4000 --keep-order "perl ./nts-scripts/detokenizer.perl -q -l de"
 else
     <nts.tmp.bpe parallel --pipe -L $SPLIT -N1 --keep-order "MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 ./bin/NiuTensor -dev -1 -sbatch 64 -wbatch 2048 -model ./model/model.fp32 -srcvocab ./model/vocab.txt -tgtvocab ./model/vocab.txt | perl ./nts-scripts/detokenizer.perl -q -l de"
 fi
