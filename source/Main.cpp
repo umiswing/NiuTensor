7ef3c3e64c97f78f47c5913155cb252582d06594
huchi <huchinlp@gmail.com>
2021-11-06 14:51:28 +0800

    no message

--- source/Main.cpp
+++ source/Main.cpp
@@ -16,47 +16,53 @@
  */
 
 /*
- * $Created by: XIAO Tong (xiaotong@mail.neu.edu.cn) 2018-07-10
+ * $Created by: Chi Hu (huchinlp@gmail.com) 2021-11-06
  */
-
-#include "./train/TTrain.h"
-#include "./tensor/test/Test.h"
-#include "./sample/fnnlm/FNNLM.h"
-#include "./sample/transformer/NMT.h"
 
 #include <iostream>
 
-using namespace nts;
-using namespace fnnlm;
+#include "./nmt/Config.h"
+#include "./nmt/train/Trainer.h"
+#include "./nmt/translate/Translator.h"
+
 using namespace nmt;
 
-int main( int argc, const char ** argv )
+int Main(int argc, const char** argv)
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    XConfig config;
+    if (argc == 0)
+        return 1;
 
-    if(argc > 1){
-        config.Create(argc - 1, argv + 1);
-        verboseLevel = config.GetInt("verbose", 1);
+    /* load configurations */
+    NMTConfig config(argc, argv);
+
+    srand(config.common.seed);
+
+    /* training */
+    if (strcmp(config.training.trainFN, "") != 0) {
+
+        NMTModel model;
+        model.InitModel(config);
+
+        Trainer trainer;
+        trainer.Init(config, model);
+        trainer.Run();
     }
 
-    if (argc > 1 && !strcmp(argv[1], "-test"))
-        Test();
-    else if (argc > 1 && !strcmp(argv[1], "-testtrain"))
-        TestTrain();
-    else if(argc > 1 && !strcmp(argv[1], "-fnnlm"))
-        FNNLMMain(argc - 1, argv + 1);
-    else if(argc > 1 && !strcmp(argv[1], "-t2t"))
-        NMTMain(argc - 1, argv + 1);
-    else{
-        fprintf(stderr, "Thanks for using NiuTensor! This is a library for building\n");
-        fprintf(stderr, "neural networks in an easy way. \n\n");
-        fprintf(stderr, "   Run this program with \"-test\" for unit test!\n");
-        fprintf(stderr, "Or run this program with \"-testtrain\" for test of the trainer!\n");
-        fprintf(stderr, "Or run this program with \"-fnnlm\" for sample FNNLM!\n");
-        fprintf(stderr, "Or run this program with \"-t2t\" for sample Transformer!\n");
+    /* translating */
+    else {
+
+        /* disable gradient flow */
+        DISABLE_GRAD;
+
+        NMTModel model;
+        model.InitModel(config);
+
+        Translator translator;
+        translator.Init(config, model);
+        translator.Translate();
     }
 
     return 0;
